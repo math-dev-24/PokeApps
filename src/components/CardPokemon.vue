322ca640efc49290879588ff5200a8b913059b49
Mathieu <mathieu.busse@live.fr>
2023-04-02 11:06:04 +0200

    update design home page 2

--- src/components/CardPokemon.vue
+++ src/components/CardPokemon.vue
@@ -11,31 +11,59 @@ const props = defineProps<{
 
 
 <template>
-  <router-link :to="`/detail/${props.pokemon.name}`" class="block border border-sky-900 rounded p-5 m-4 flex-auto" :class="props.color" id="card">
-    <img :src="props.pokemon.image" alt="pokemonImage" class="w-32 m-auto">
-    <h1
-        class="text-center text-xl font-semibold my-2 block"
-    >
-      {{props.pokemon.name}}
-    </h1>
-    <div class="flex content-between w-full items-center">
+  <router-link
+      :to="`/detail/${props.pokemon.name}`"
+      class="block border border-sky-900 rounded p-5 m-4 flex-auto"
+      :class="props.color"
+      id="card">
+
+    <div class="id">
+      #{{props.pokemon.id}}
+    </div>
+    <img :src="props.pokemon.image" alt="pokemonImage" class="img">
+
+
+    <div class="flex flex-col content-start">
+      <h1 class="text-xl font-semibold my-2 block">
+        {{props.pokemon.name}}
+      </h1>
       <template v-for="type in pokemon.apiTypes">
-        <div class="flex flex-col flex-1 text-center">
-          <div>{{type.name}}</div>
-          <img :src="type.image" alt="" class="w-7 m-auto">
+        <div class="type">
+          <img :src="type.image" alt="" class="w-7">
+          <div class="ml-2 font-semibold">{{type.name}}</div>
         </div>
       </template>
     </div>
-
   </router-link>
 </template>
 
 <style scoped lang="sass">
 #card
-  box-shadow: rgba(0, 0, 0, 0.3) 0px 6px 12px
+  box-shadow: rgba(0, 0, 0, 0.3) 0 6px 12px
   transition: all 0.2s
+  height: 220px
+  width: 350px
+  position: relative
   &:hover
-    box-shadow: rgba(0, 0, 0, 0.3) 0px 2px 5px
+    box-shadow: rgba(0, 0, 0, 0.3) 0 2px 5px
+  .type
+    display: flex
+    width: 40%
+    padding: 5px
+    margin: 10px
+    background: rgba(0,0,0,0.1)
+    border-radius: 20px
 
+  .id
+    position: absolute
+    right: 10px
+    font-size: 1.5em
+    color: #9f9693
+    top: 1px
+  .img
+    max-width: 160px
+    position: absolute
+    right: 3px
+    bottom: 2px
 
 </style>
